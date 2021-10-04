8206f3a09cc03230bb4bcc996c09e5ea266bfa0a
BazaochiDumitru <bazaochi.dumitru@mail.ru>
2021-10-04 19:16:31 +0300

    Create table name and columns in Country.java

--- src/main/java/com/example/HibernateExampleHw/model/Country.java
+++ src/main/java/com/example/HibernateExampleHw/model/Country.java
@@ -1,4 +1,20 @@
 package com.example.HibernateExampleHw.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "countries")
 public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "code")
+    private String code;
 }
