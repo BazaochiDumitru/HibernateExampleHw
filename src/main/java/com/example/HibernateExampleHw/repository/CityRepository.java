a8bb35b4fb34deff68a78ea5d6f439ecae89afd8
BazaochiDumitru <bazaochi.dumitru@mail.ru>
2021-10-04 20:42:39 +0300

    Extends JpaRepository for CityRepository

--- src/main/java/com/example/HibernateExampleHw/repository/CityRepository.java
+++ src/main/java/com/example/HibernateExampleHw/repository/CityRepository.java
@@ -1,4 +1,7 @@
 package com.example.HibernateExampleHw.repository;
 
-public class CityRepository {
+import com.example.HibernateExampleHw.model.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Long> {
 }
