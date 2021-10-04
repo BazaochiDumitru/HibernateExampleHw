5ea30e03399b2473c59a09199a97248a9501d4d3
BazaochiDumitru <bazaochi.dumitru@mail.ru>
2021-10-04 19:52:28 +0300

    Method Get and GetOneById done

--- src/main/java/com/example/HibernateExampleHw/controller/CountryController.java
+++ src/main/java/com/example/HibernateExampleHw/controller/CountryController.java
@@ -4,10 +4,9 @@ import com.example.HibernateExampleHw.model.Country;
 import com.example.HibernateExampleHw.service.CountryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/countries")
@@ -22,6 +21,16 @@ public class CountryController {
     @PostMapping
     public ResponseEntity<Country> create(@RequestBody Country country) {
         return new ResponseEntity<Country>(countryService.save(country), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Country> get() {
+        return countryService.getAll();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Country> get(@PathVariable("id") long id) {
+        return new ResponseEntity<Country>(countryService.get(id), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/HibernateExampleHw/service/impl/CountryServiceImpl.java
+++ src/main/java/com/example/HibernateExampleHw/service/impl/CountryServiceImpl.java
@@ -1,9 +1,12 @@
 package com.example.HibernateExampleHw.service.impl;
 
+import com.example.HibernateExampleHw.exception.ResourceNotFoundException;
 import com.example.HibernateExampleHw.model.Country;
 import com.example.HibernateExampleHw.repository.CountryRepository;
 import com.example.HibernateExampleHw.service.CountryService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryServiceImpl implements CountryService {
@@ -17,5 +20,15 @@ public class CountryServiceImpl implements CountryService {
     @Override
     public Country save(Country country) {
         return countryRepository.save(country);
+    }
+
+    @Override
+    public List<Country> getAll() {
+        return countryRepository.findAll();
+    }
+
+    @Override
+    public Country get(long id) {
+        return countryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Country", "id", id));
     }
 }
