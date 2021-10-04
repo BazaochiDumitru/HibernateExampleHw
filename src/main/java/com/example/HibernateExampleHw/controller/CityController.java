325ab266ed291fe273cf62477f5489ee0eddd861
BazaochiDumitru <bazaochi.dumitru@mail.ru>
2021-10-04 20:44:56 +0300

    Add entity, table name and column name for City

--- src/main/java/com/example/HibernateExampleHw/controller/CityController.java
+++ src/main/java/com/example/HibernateExampleHw/controller/CityController.java
@@ -1,17 +1,4 @@
 package com.example.HibernateExampleHw.controller;
 
-import lombok.Data;
-
-import javax.persistence.*;
-
-@Data
-@Entity
-@Table(name = "cities")
 public class CityController {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
-    @Column(name = "name")
-    private String name;
 }
