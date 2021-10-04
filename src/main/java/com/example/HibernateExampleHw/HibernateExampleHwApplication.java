4bb71d0fcd7a7d8e72f42014c83b1bf5f5fae512
BazaochiDumitru <bazaochi.dumitru@mail.ru>
2021-10-04 18:51:32 +0300

    Initial commit

---
+++ src/main/java/com/example/HibernateExampleHw/HibernateExampleHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.HibernateExampleHw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateExampleHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateExampleHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HibernateExampleHw/HibernateExampleHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HibernateExampleHw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateExampleHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
