e691ca878104c51c0211f3d19d460b206f89e393
BazaochiDumitru <bazaochi.dumitru@mail.ru>
2021-10-04 19:44:00 +0300

    Method Create done

--- src/main/java/com/example/HibernateExampleHw/service/CountryService.java
+++ src/main/java/com/example/HibernateExampleHw/service/CountryService.java
@@ -1,4 +1,9 @@
 package com.example.HibernateExampleHw.service;
 
+import com.example.HibernateExampleHw.model.Country;
+
+import java.util.List;
+
 public interface CountryService {
+    Country save(Country country);
 }
--- src/main/java/com/example/HibernateExampleHw/service/impl/CountryServiceImpl.java
+++ src/main/java/com/example/HibernateExampleHw/service/impl/CountryServiceImpl.java
@@ -1,4 +1,21 @@
 package com.example.HibernateExampleHw.service.impl;
 
-public class CountryServiceImpl {
+import com.example.HibernateExampleHw.model.Country;
+import com.example.HibernateExampleHw.repository.CountryRepository;
+import com.example.HibernateExampleHw.service.CountryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CountryServiceImpl implements CountryService {
+    CountryRepository countryRepository;
+
+    public CountryServiceImpl(CountryRepository countryRepository) {
+        super();
+        this.countryRepository = countryRepository;
+    }
+
+    @Override
+    public Country save(Country country) {
+        return countryRepository.save(country);
+    }
 }
